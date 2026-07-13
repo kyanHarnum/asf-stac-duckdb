3b01360fcbb22eeee7b3a344efa3fce3c517d913
Kyan Harnum <kyan79@outlook.com>
2026-07-13 12:58:46 -0800

    Testing conversion, installation action as well as added the asf-stac-util folder from asf-stac for creating items

---
+++ asf-stac-util/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='asf-stac-util',
+    license='BSD',
+    include_package_data=True,
+    install_requires=[],
+    python_requires='~=3.9',
+    packages=find_packages(),
+)
