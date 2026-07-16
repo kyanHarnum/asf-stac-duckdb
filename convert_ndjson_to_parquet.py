b08f74c7126069e21178ae4da7a015f8377bd0aa
Kyan Harnum <kyan79@outlook.com>
2026-07-16 11:06:40 -0800

    configured maximum object size for duckdb conversion

--- convert_ndjson_to_parquet.py
+++ convert_ndjson_to_parquet.py
@@ -4,6 +4,9 @@ import duckdb
 ndjson_path = sys.argv[1]
 parquet_path = sys.argv[2]
 
+
 con = duckdb.connect()
+
+con.sql(f"SELECT * FROM read_json_auto('{ndjson_path}', maximum_object_size=1000000000)")
 con.sql(f"COPY (SELECT * FROM read_json_auto('{ndjson_path}')) TO '{parquet_path}' (FORMAT 'parquet')")
 print(f"Wrote {parquet_path}")
